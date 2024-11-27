ed5978048b994c54b6b3e469719979c0b7598663
Aleksey Khvalko <ari.khvalko@gmail.com>
2024-11-27 14:15:58 +0300

    add homepage

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+
+const Navbar = () => {
+    return (
+        <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+            <MaxWidthWrapper>
+                <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                    <Link href='/' className="flex z-40 font-semibold">
+                        <span>Ari's portfolio</span>
+                    </Link>
+
+                    <div className="hidden items-center space-x-4 text-[18px] sm:flex hover:opacity-75">
+                        <>
+                            <Link href='#'>Home
+                            </Link>
+
+                            <Link href='#'>
+                                About me
+                            </Link>
+
+                            <Link href='#'>
+                                Projects
+                            </Link>
+                        </>
+                    </div>
+                </div>
+            </MaxWidthWrapper>
+        </nav>
+    )
+}
+
+export default Navbar
